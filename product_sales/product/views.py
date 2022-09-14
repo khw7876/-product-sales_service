74e6a3de5aba8be78a0433f83f0753d4d5759f25
khw7876 <khw7876@gmail.com>
2022-09-15 00:38:22 +0900

    refac/ 상품등록 api 수정

--- product_sales/product/views.py
+++ product_sales/product/views.py
@@ -4,11 +4,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Create your views here.
+from .services.product_service import (
+    create_product,
+)
 
 class ProductView(APIView):
     """
     상품에 관련된 View
     """
     def post(self, request):
-        
+        create_product(request.data, request.user)
         return Response({"detail" : "상품을 등록하였습니다."}, status=status.HTTP_200_OK)
