70cca13f5015f73630300df1390a27b1caed78e4
khw7876 <khw7876@gmail.com>
2022-09-14 23:59:34 +0900

    feat/ 유저 포인트 충전 API 구현

--- product_sales/user/urls.py
+++ product_sales/user/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.UserView.as_view(), name="user_view"),
+    path("charge", views.ChargePointView.as_view(), name="charge_point_view"),
     path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
