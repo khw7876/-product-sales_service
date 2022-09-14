f791eb54c96167fb6c1d6f70c3a9666f4cd28281
khw7876 <khw7876@gmail.com>
2022-09-15 00:13:16 +0900

    feat/ product model 생성

--- product_sales/product/models.py
+++ product_sales/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField("상품이름", max_length=40)
+    price = models.IntegerField("상품가격")
+    delivery_fee = models.IntegerField("배송비")
+
+    def __str__(self):
+        return self.name
--- product_sales/product/views.py
+++ product_sales/product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class ProductView(APIView):
+    """
+    상품에 관련된 View
+    """
+    def post(self, request):
+        
+        return Response({"detail" : "상품을 등록하였습니다."}, status=status.HTTP_200_OK)
